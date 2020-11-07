6aa3233ba9dbcb17f2b87618e357a9a157fb7162
Jahangir <jahangirhossainm69@gmail.com>
2020-11-08 03:11:45 +0600

    Added Features

--- app/src/main/java/com/beffjezos/carebuddy/MainActivity.java
+++
@@ -1,1 +0,0 @@
-package com.beffjezos.carebuddy;import androidx.appcompat.app.AppCompatActivity;import android.os.Bundle;public class MainActivity extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_main);    }}
